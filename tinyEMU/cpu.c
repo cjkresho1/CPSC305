f72251a22950b7d888928228dbbbdafe96ce1633
Charles Kresho <ckresho@mail.umw.edu>
2020-11-25 19:27:36 -0500

    Added show_regs functionality successfully

--- tinyEMU/cpu.c
+++ tinyEMU/cpu.c
@@ -32,7 +32,10 @@ int get_cpsr() {
 }
 
 void show_regs() {
-    printf("You need to implement show_regs().\n");
+    printf("R0:  0x%.8X, R1:  0x%.8X, R2:  0x%.8X, R3:  0x%.8X\n", registers[0], registers[1], registers[2], registers[3]);
+    printf("R4:  0x%.8X, R5:  0x%.8X, R6:  0x%.8X, R7:  0x%.8X\n", registers[4], registers[5], registers[6], registers[7]);
+    printf("R8:  0x%.8X, R9:  0x%.8X, R10: 0x%.8X, R11: 0x%.8X\n", registers[8], registers[9], registers[10], registers[11]);
+    printf("R12: 0x%.8X, R13: 0x%.8X, R14: 0x%.8X, R15: 0x%.8X\n", registers[12], registers[13], registers[14], registers[15]);
 }
 
 void step() {
